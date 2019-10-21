c37bf06d5618c1e806827fd1bcde0547d21464f6
jubatus <jubatus.vc@gmail.com>
2019-10-22 00:12:05 +0200

    jugadores nba añadidos

---
+++ ejercicio02/jugadoresNBA.sql
@@ -0,0 +1,23 @@
+
+/* Insertando datos */
+
+insert into jugadores (nombre, equipo, puntos, minutos, posicion) values
+    ('Bryant, Kobe', 'LA Lakers', 28.3, 38.9, 'Alero'),
+    ('Curry, Stephen', 'Golden State', 27.3, 33.8, 'Base'),
+    ('Gasol, Marc', 'Toronto', 16.6, 33.2, 'Pivot'),
+    ('Ainge, Danny', 'Boston', 15.9, 30.0, 'Escolta'),
+    ('Bird, Larry', 'Boston', 28.1, 38.4, 'Alero'),
+    ('Jordan, Michael', 'Chicago', 31.5, 39.0, 'Escolta'),
+    ('Thomas, Isiah', 'Detroit', 19.4, 36.3, 'Base'),
+    ('Mullin, Chris', 'Golden State', 29.8, 37.6, 'Escolta'),
+    ('King, Bernard', 'New York', 32.9, 37.5, 'Escolta'),
+    ('Walton, Bill', 'Boston', 13.9, 33.3, 'Pivot'),
+    ('Barkley, Charles', 'Philadelphia', 27.6, 38.4, 'Ala-Pivot'),
+    ('Maravich, Pete', 'Atlanta', 27.7, 37.0, 'Escolta'),
+    ('Johnson, Magic', 'LA Lakers', 19.6, 38.3, 'Base'),
+    ('Olajuwon, Hakeem', 'Houston', 27.3, 40.9, 'Pivot'),
+    ('Stockton, John', 'Utah', 14.7, 35.3, 'Ala-Pivot'),
+    ('Durant, Kevin', 'Golden State', 28.2, 36.9, 'Alero'),
+    ('Ewing, Patrick', 'New York', 30.1, 40.9, 'Ala-Pivot'),
+    ('Malone, Karl', 'Utah', 24.5, 37.6, 'Pivot');
+
